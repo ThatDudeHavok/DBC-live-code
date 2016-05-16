bfd9931fe832048d17f631890b21232206054865
Nicholas J. Olson <nicholasjamesolson@gmail.com>
2016-05-15 21:12:20 -0500

    Add rooms keys with empty array values to house hash

--- house-builder/house_maker.rb
+++ house-builder/house_maker.rb
@@ -9,4 +9,19 @@
 
 # BUSINESS LOGIC
 
+def add_room_to_house(house, room_name)
+	house[room_name] = []
+end
+
+house = {}
+
+# TEST CODE
+
+rooms = ["Living room", "bedroom", "bathroom", "kitchen"]
+
+rooms.each do |room|
+	add_room_to_house(house, room)
+end
+p house
+
 # USER INTERFACE
