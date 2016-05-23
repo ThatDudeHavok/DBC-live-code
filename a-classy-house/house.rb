431b86d091db9d0fc77560900127fd2ddba27b6f
Nicholas J Oson <nicholasjamesolson@gmail.com>
2016-05-23 10:57:59 -0500

    Move room class design pseudocode from house file to room file

--- a-classy-house/house.rb
+++ a-classy-house/house.rb
@@ -11,15 +11,3 @@
 # - total_value (ads up the value of items
 # in all rooms)
 # - to_s override
-
-# ROOM CLASS
-# Attributes:
-# - name
-# - width
-# - length
-# - items (a collection of Item instances)
-# Methods:
-# - getters for items, name, width, length
-# - setter for items, name
-# - total value (adds up prices of items)
-# - to_s override
