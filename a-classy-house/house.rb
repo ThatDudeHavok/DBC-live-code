9f76b26ab1f318842fc1823f71d9a8939437c0e3
Nicholas J Oson <nicholasjamesolson@gmail.com>
2016-05-23 10:49:02 -0500

    Write pseudocode for class structures

---
+++ a-classy-house/house.rb
@@ -0,0 +1,34 @@
+# Build a house out of classes
+
+# HOUSE CLASS
+# Attributes:
+# - rooms (a collection of Romm instances)
+# Methods:
+# - getter for rooms
+# - add_room (only allows up to 10 rooms)
+# - square_footage(adds up the house's
+# square footage and returns it as an integer)
+# - total_value (ads up the value of items
+# in all rooms)
+# - to_s override
+
+# ROOM CLASS
+# Attributes:
+# - name
+# - width
+# - length
+# - items (a collection of Item instances)
+# Methods:
+# - getters for items, name, width, length
+# - setter for items, name
+# - total value (adds up prices of items)
+# - to_s override
+
+# ITEM CLASS
+# Attributes:
+# - name
+# - color
+# - price
+# Methods:
+# - getters for color, price, name
+# - to_s override
