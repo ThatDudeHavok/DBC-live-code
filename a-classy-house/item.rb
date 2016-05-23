2556bba29a5a1eeffd787e55ec387776ec326b0b
Nicholas J Oson <nicholasjamesolson@gmail.com>
2016-05-23 11:02:53 -0500

    Declare Item class and primary attributes with initialize method

--- a-classy-house/item.rb
+++ a-classy-house/item.rb
@@ -6,3 +6,18 @@
 # Methods:
 # - getters for color, price, name
 # - to_s override
+
+# Item is a super vague class name, something like
+# furniture would probably be a better idea. But,
+# item was used in the last live code 'house builder'
+# or something of that manner. So, we're gonna stick
+# with the naming convention.
+class Item
+	attr_reader :name, :color, :price
+
+	def initialize(name, color, price)
+		@name = name
+		@color = color
+		@price = price
+	end
+end
